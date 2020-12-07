5a85c085f94eafc90e6d20963de4cb5f4c61af97
GermanBluefox <dogafox@gmail.com>
2020-12-08 00:05:56 +0100

    Fix example

--- main.js
+++ main.js
@@ -90,7 +90,7 @@ function startAdapter(options) {
 async function main() {
 
     // Reset the connection indicator during startup
-    await this.setStateAsync('info.connection', false, true);
+    await adapter.setStateAsync('info.connection', false, true);
 
     // The adapters config (in the instance object everything under the attribute "native") is accessible via
     // adapter.config:
